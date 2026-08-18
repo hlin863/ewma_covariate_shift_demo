ad223ede6b79d8b5f603fc6bfdedc70b52e99b25
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2026-08-18 13:48:52 +0100

    Add reporting package

---
+++ src/reporting/__init__.py
@@ -0,0 +1,15 @@
+"""Experiment result formatting and reporting helpers."""
+
+from src.reporting.table1 import (
+    Table1Row,
+    comparison_dataframe,
+    paper_style_dataframe,
+    paper_style_markdown,
+)
+
+__all__ = [
+    "Table1Row",
+    "comparison_dataframe",
+    "paper_style_dataframe",
+    "paper_style_markdown",
+]
