155989081a8c304eeb605bed5fa39a381a584942
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2026-08-18 13:47:39 +0100

    Group Dataset 2A functionality

---
+++ src/bci/datasets/dataset2a/__init__.py
@@ -0,0 +1,37 @@
+"""BCI Competition IV Dataset 2A reproduction helpers."""
+
+from src.bci_2a_experiment import (
+    DATASET_2A_CHANNELS,
+    DATASET_2A_EEG_MONTAGE,
+    PUBLISHED_2A_RESULTS,
+    Dataset2AExperimentResult,
+    Dataset2AFeaturePipelineResult,
+    Dataset2ATrialFeatureResult,
+    Dataset2ATrialSignalResult,
+    build_dataset_2a_fbcsp_features,
+    extract_dataset_2a_trials,
+    run_dataset_2a_subject,
+    split_dataset_2a_session1,
+)
+from src.bci_2a_development_pipeline import (
+    Dataset2ADevelopmentFeaturePipelineResult,
+    build_dataset_2a_development_fbcsp_features,
+)
+from src.bci_2a_development_split import Dataset2ADevelopmentSplit
+
+__all__ = [
+    "DATASET_2A_CHANNELS",
+    "DATASET_2A_EEG_MONTAGE",
+    "PUBLISHED_2A_RESULTS",
+    "Dataset2ADevelopmentFeaturePipelineResult",
+    "Dataset2ADevelopmentSplit",
+    "Dataset2AExperimentResult",
+    "Dataset2AFeaturePipelineResult",
+    "Dataset2ATrialFeatureResult",
+    "Dataset2ATrialSignalResult",
+    "build_dataset_2a_development_fbcsp_features",
+    "build_dataset_2a_fbcsp_features",
+    "extract_dataset_2a_trials",
+    "run_dataset_2a_subject",
+    "split_dataset_2a_session1",
+]
