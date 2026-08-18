f24d66e8eaacdfce8f98fc4ccf183e2fcbce8812
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2026-08-18 14:00:34 +0100

    Point Stage-I package at canonical EWMA modules

--- src/detection/stage1/__init__.py
+++ src/detection/stage1/__init__.py
@@ -1,6 +1,6 @@
 """Stage-I EWMA warning detectors."""
 
-from src.ewma import (
+from src.detection.stage1.sd_ewma import (
     EWMATrainingResult,
     SD_EWMA_Config,
     calculate_ewma_training_path,
@@ -8,13 +8,19 @@ from src.ewma import (
     fit_sd_ewma,
     run_sd_ewma,
 )
-from src.msd_ewma import MSDTrainingResult, fit_msd_ewma, run_msd_ewma
+from src.detection.stage1.msd_ewma import (
+    MSDTrainingResult,
+    calculate_multivariate_ewma_path,
+    fit_msd_ewma,
+    run_msd_ewma,
+)
 
 __all__ = [
     "EWMATrainingResult",
     "MSDTrainingResult",
     "SD_EWMA_Config",
     "calculate_ewma_training_path",
+    "calculate_multivariate_ewma_path",
     "estimate_lambda",
     "fit_msd_ewma",
     "fit_sd_ewma",
