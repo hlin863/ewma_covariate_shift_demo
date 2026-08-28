2dfce1148131ae5cd15d3789b3d93f876e0120b5
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2026-08-28 22:33:16 +0100

    Export repeated-shift evaluation API

--- src/reporting/__init__.py
+++ src/reporting/__init__.py
@@ -1,6 +1,12 @@
 """Experiment result formatting and evaluation helpers."""
 
-from src.reporting.metrics import DetectionMetrics, evaluate_stage_1_detection
+from src.reporting.metrics import (
+    DetectionMetrics,
+    RepeatedShiftEvaluation,
+    RepeatedShiftMetrics,
+    evaluate_repeated_shift_detection,
+    evaluate_stage_1_detection,
+)
 from src.reporting.table1 import (
     Table1Row,
     comparison_dataframe,
@@ -10,8 +16,11 @@ from src.reporting.table1 import (
 
 __all__ = [
     "DetectionMetrics",
+    "RepeatedShiftEvaluation",
+    "RepeatedShiftMetrics",
     "Table1Row",
     "comparison_dataframe",
+    "evaluate_repeated_shift_detection",
     "evaluate_stage_1_detection",
     "paper_style_dataframe",
     "paper_style_markdown",
