7f160ce67c34de6c42c5a92912031afe70b0828a
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2026-08-28 22:32:52 +0100

    Add src/experiments/paper2015/__init__.py

---
+++ src/experiments/paper2015/__init__.py
@@ -0,0 +1,15 @@
+"""Reproduction experiments for Raza, Prasad and Li (2015)."""
+
+from src.experiments.paper2015.d2 import (
+    D2ExperimentConfig,
+    D2ExperimentResult,
+    PAPER_D2_REFERENCE,
+    run_d2_experiment,
+)
+
+__all__ = [
+    "D2ExperimentConfig",
+    "D2ExperimentResult",
+    "PAPER_D2_REFERENCE",
+    "run_d2_experiment",
+]
